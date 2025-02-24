a3bc2f82ac5f029dc0c0cbb3e2f0e7cee99dd145
Salaev <Aleksey.Salaev@rosbank.ru>
2025-02-24 13:35:29 +0300

    Добавлен docker-compose.yml

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 prometheus http://localhost:9090
 grafana http://localhost:3000
 admin http://localhost:8084/
+api loki https://grafana.com/docs/loki/latest/reference/loki-http-api/
+api tempo https://grafana.com/docs/tempo/latest/api_docs/
 
-дашборды для графаны https://grafana.com/grafana/dashboards/
-дашборды jvm https://grafana.com/grafana/dashboards/4701-jvm-micrometer/
+дашборды (https://grafana.com/grafana/dashboards/)
+ - jvm https://grafana.com/grafana/dashboards/4701-jvm-micrometer/
+ - observability https://grafana.com/grafana/dashboards/17175-spring-boot-observability/
--- actuator/src/main/java/ru/otus/actuator/ActuatorController.java
+++ actuator/src/main/java/ru/otus/actuator/ActuatorController.java
@@ -1,5 +1,7 @@
 package ru.otus.actuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,17 @@ import java.util.Random;
 @RequestMapping("/api")
 public class ActuatorController {
 
+    private final Logger logger = LoggerFactory.getLogger(ActuatorController.class);
+
     private final Random random = new Random();
     double lambda = 0.005;
 
     @GetMapping("/hello")
     public String sayHello() throws InterruptedException {
+        logger.info("Info{}", random.nextInt());
+        logger.debug("Debug{}", random.nextInt());
+        logger.warn("Warn{}", random.nextInt());
+        logger.error("Error{}", random.nextInt());
         long delay = getDelay();
         Thread.sleep(delay);
         return "Привет, Otus! (Delay: " + delay + ")";
